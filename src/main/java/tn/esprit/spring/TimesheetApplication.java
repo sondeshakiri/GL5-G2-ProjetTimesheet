87cf74eb89b5418ecf36634399d3c9796c36759a
M-Said-C <chaiebsaid.01@gmail.com>
2024-10-19 20:17:06 +0100

    modif pom.xml

--- src/main/java/tn/esprit/spring/TimesheetApplication.java
+++ src/main/java/tn/esprit/spring/TimesheetApplication.java
@@ -6,14 +6,13 @@ import javax.faces.webapp.FacesServlet;
 import javax.servlet.DispatcherType;
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class TimesheetApplication {
 
 	public static void main(String[] args) {SpringApplication.run(TimesheetApplication.class, args);}
