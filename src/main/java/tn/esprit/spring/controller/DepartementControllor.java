473bd4389f6c419b932a42291b5f19cc04b803fd
sondes hakiri <154908605+sondeshakiri@users.noreply.github.com>
2024-10-27 08:42:28 +0100

    Add API endpoints for Departement management

--- src/main/java/tn/esprit/spring/controller/DepartementControllor.java
+++ src/main/java/tn/esprit/spring/controller/DepartementControllor.java
@@ -3,7 +3,6 @@ package tn.esprit.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.services.DepartementService;
 
@@ -16,30 +15,35 @@ public class DepartementControllor {
     @Autowired
     private DepartementService departementService;
 
-    @GetMapping
+    // API pour obtenir tous les départements
+    @GetMapping("/all")
     public List<Departement> getAllDepartements() {
         return departementService.findAll();
     }
 
-    @GetMapping("/{id}")
+    // API pour obtenir un département par son ID
+    @GetMapping("/get/{id}")
     public ResponseEntity<Departement> getDepartementById(@PathVariable int id) {
         Departement departement = departementService.findById(id);
         return ResponseEntity.ok(departement);
     }
 
-    @PostMapping
+    // API pour créer un nouveau département
+    @PostMapping("/create")
     public Departement createDepartement(@RequestBody Departement departement) {
         return departementService.save(departement);
     }
 
-    @PutMapping("/{id}")
+    // API pour mettre à jour un département existant
+    @PutMapping("/update/{id}")
     public ResponseEntity<Departement> updateDepartement(@PathVariable int id, @RequestBody Departement departement) {
         departement.setId(id);
         Departement updatedDepartement = departementService.update(departement);
         return ResponseEntity.ok(updatedDepartement);
     }
 
-    @DeleteMapping("/{id}")
+    // API pour supprimer un département par son ID
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteDepartement(@PathVariable int id) {
         departementService.delete(id);
         return ResponseEntity.noContent().build();
