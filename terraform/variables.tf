0e8d3091e27135547d1998859a690e0a44b67f2c
sondes hakiri <sondes54hakiri@gmail.com>
2024-11-02 13:09:11 +0100

    modifier les variable d'env

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,29 +7,29 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
+  default     = "mykubernetes" 
 }
 
 variable "subnet_ids" {
   description = "IDs des sous-réseaux"
   type        = list(string)
-  default     = ["subnet-095630f08531fac7e", "subnet-002c5cca8ae61903f"]  # Valeurs par défaut
+  default     = ["subnet-095630f08531fac7e", "subnet-002c5cca8ae61903f"] 
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::077108977568:role/LabRole"  # Valeur par défaut
+  default     = "arn:aws:iam::077108977568:role/LabRole"  
 }
 
 variable "vpc_id" {
   description = "L'ID du VPC pour le cluster EKS"
   type        = string
-  default     = "vpc-081470f81281db969"  # Remplacez par votre ID de VPC réel
+  default     = "vpc-081470f81281db969" 
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
+  default     = "10.0.0.0/16"  
 }
