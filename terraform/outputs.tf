e7e43d0f9b81eb9293c95f9c2546fcd1cd2a038f
LENOVO <merriambelhadjhamida@gmail.com>
2024-11-08 21:37:32 +0100

    corrected typo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
 
 output "public_subnet_id" {
   description = "The ID of the public subnet"
-  value       = aws_subnet.public_subnet.id
+  value       = aws_subnet.public_subnet1.id
 }
 
 output "private_subnet_1_id" {
